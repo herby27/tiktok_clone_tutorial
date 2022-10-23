87f01196e1a9b0f09191dd724726637e43ada7f3
mmsinno <stephherbert@yahoo.com>
2022-10-22 20:02:54 -0400

    10/22

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,12 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone_tutorial/views/widgets/screens/add_video_screen.dart';
+import 'package:tiktok_clone_tutorial/views/widgets/screens/video_screen.dart';
 import 'controllers/auth_controller.dart';
 
-const pages = [
-  Text('Home Screen'),
+List pages = [
+  VideoScreen(),
   Text('Search Screen'),
-  Text('Add Page Screen'),
+  const AddVideoScreen(),
   Text('Messages Screen'),
   Text('Profile'),
 ];
